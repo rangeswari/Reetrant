db92a887529518bc720b28d61f3f111255b796de
rangeswari <arouf21@gmail.com>
2019-02-25 20:04:57 +0530

    RabbitMQ

---
+++ springboot-rabbitmq-demo/src/main/java/com/sivalabs/sringboot/rabbitmq/SpringbootRabbitmqDemoApplication.java
@@ -0,0 +1,14 @@
+package com.sivalabs.sringboot.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRabbitmqDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRabbitmqDemoApplication.class, args);
+	}
+
+}
+
